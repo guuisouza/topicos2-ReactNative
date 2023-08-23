c484db9c9a55dacf2959d866370b2ac56c9d7a68
Guilherme Souza <89078191+guuisouza@users.noreply.github.com>
2023-08-23 09:05:30 -0300

    Add files via upload

--- topicos2-20232/App.js
+++ topicos2-20232/App.js
@@ -3,7 +3,8 @@ import HelloWorld2 from './components/hello-world/HelloWorld2'
 import Light from './components/light/Light'
 import Login from './components/login-page/Login'
 import Facebook from './components/login-page/Facebook'
+import RegisterForm from './components/register-form/RegisterForm'
 export default function App() {
-  return <Facebook />
+  return <RegisterForm />
 
 }
---
+++ topicos2-20232/components/register-form/RegisterForm.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native'
+
+export default function RegisterForm() {
+  return(
+    <View> 
+      <Text>Formulário de Registro</Text>
+    </View>
+  )
+
+
+
+
+
+
+}
